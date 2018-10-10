83544083bff3a2d620287f0b98652fc59a6cd21b
CfortWang <cfortwang@gmail.com>
2018-10-10 14:10:04 +0800

    cfort

--- resources/views/web/contents/event/coupon.blade.php
+++ resources/views/web/contents/event/coupon.blade.php
@@ -3,7 +3,6 @@
 @section('css')
 <link rel="stylesheet" href="/css/app.css">
 <link rel="stylesheet" type="text/css" media="all" href="/css/daterangepicker.css" />
-<link rel="stylesheet" type="text/css" media="all" href="/css/bootstrap-datetimepicker.min.css" />
 @endsection('css')
 @section('content')
     <div class="tpl-page-container tpl-page-header-fixed">
@@ -13,14 +12,9 @@
                     <div class="tpl-portlet">
                         <div class="tpl-portlet-title">
                             <div class="search-box">
-                                <input type="text" class="search-input" name="search" placeholder="请输入手机号查找">
+                                <input type="text" class="search-input" name="search" placeholder="请输入商品名称查找">
                                 <button class="search-btn">搜索</button>
                             </div>
-                            <!-- <select data-am-selected>
-                                <option value="1" selected>进行中</option>
-                                <option value="2">未开始</option>
-                                <option value="3">已结束</option>
-                            </select> -->
                             <div class="create-pdd">
                                 <span>新建拼豆豆</span>
                             </div>
@@ -41,15 +35,6 @@
                                 </div>
                                 <div class="table-content"></div>
                             </div>
-                            <div class="pagination">
-                                <div class="page-down">
-                                    <img src="/img/main/icon_page_left.png" alt="">
-                                </div>
-                                <div class="page-number">1</div>
-                                <div class="page-up">
-                                    <img src="/img/main/icon_page_right.png" alt="">
-                                </div>
-                            </div>
                         </div>
                     </div>
                 </div>
@@ -59,62 +44,53 @@
 @endsection
 @section('script')
 <script>
-    var limit = 8
-    var type = 'scan'
-    var page = 1
     var pageCount
+    var keyword = ''
     var drawList = function () {
         $.ajax({
-            url: 'http://shop.test/api/customer/scannedUserList',
+            url: 'http://shop.test/api/event/groupon',
             type: 'get',
             dataType: 'json',
             data: {
-                type: type,
-                limit: limit,
-                page: page
+                keyword: keyword
             },
             success: function (res) {
                 $(".table-content").empty()
-                let resData = res.data.scanUserList
+                let resData = res.data
                 console.log(resData)
-                let count = res.data.count
-                pageCount = Math.ceil(count / limit)
-                console.log(pageCount)
-                var $tr = '<div class="table-tr clear-fix"><div class="table-td-id"></div><div class="table-td-nickname"></div><div class="table-td-sex"></div><div class="table-td-age"></div><div class="table-td-first"><p class="years"></p><p class="hours"></p></div><div class="table-td-last"><p class="years"></p><p class="hours"></p></div><div class="table-td-frequency"></div><div class="table-td-count"></div><div class="see-more">查看更多</div></div>'
+                var $tr = '<div class="table-tr clear-fix"><div class="table-td-name"></div><div class="table-td-price"><p class="new-price"></p><p class="old-price"><del></del></p></div><div class="table-td-effective"></div><div class="table-td-organize"></div><div class="table-td-join"></div><div class="table-td-success"></div><div class="table-td-used"></div><div class="table-td-notuse"></div><div class="table-td-rate"></div><div class="operating"><span class="shelf"></span><span class="obtained"></span><span class="modify">&nbsp;&nbsp;&nbsp;&nbsp;修改</span></div></div>'
                 for (let i = 0; i < resData.length; i++) {
                     $('.table-content').append($tr)
-                    if (resData[i].id == null || resData[i].id == '') {
-                        var id = '——'
-                    } else {
-                        var id = "+" + resData[i].id.split('@')[1] + " " + resData[i].id.split('@')[0]
+                    let groupName = resData[i].title
+                    let newPrice = "￥" + resData[i].discounted_price
+                    let oldPrice = "原价：" + resData[i].price
+                    let effective = resData[i].effective_day.split(' ')[0]
+                    let groupNum = resData[i].group
+                    let joinNum = resData[i].join_number
+                    let usedNum = resData[i].used
+                    let unusedNum = resData[i].unused
+                    let successNum = usedNum + unusedNum
+                    let rate = usedNum + '/' + successNum
+                    let status = resData[i].product_status
+                    let id = resData[i].id
+                    if (status == 0) {
+                        $(".table-content .table-tr:eq("+ i +") .operating .obtained").text("下架")
                     }
-                    let seq = resData[i].user
-                    let nickname = resData[i].nickname
-                    let gender = resData[i].gender
-                    let age = resData[i].age
-                    let firstTimeYears = resData[i].firstTime.split(' ')[0]
-                    let firstTimeHours = resData[i].firstTime.split(' ')[1]
-                    let endTimeYears = resData[i].endTime.split(' ')[0]
-                    let endTimeHours = resData[i].endTime.split(' ')[1]
-                    let percent = resData[i].rate
-                    let count = resData[i].scannedCount
-                    if (gender == null || gender == '') {
-                        gender = '——'
+                    if (status == 1) {
+                        $(".table-content .table-tr:eq("+ i +") .operating .shelf").text("上架")
                     }
-                    if (nickname == null || nickname == '') {
-                        nickname = '——'
-                    }
-                    $(".table-content .table-tr:eq("+ i +")").attr('data-seq', seq)
-                    $(".table-content .table-tr:eq("+ i +") .table-td-id").text(id)
-                    $(".table-content .table-tr:eq("+ i +") .table-td-nickname").text(nickname)
-                    $(".table-content .table-tr:eq("+ i +") .table-td-sex").text(gender)
-                    $(".table-content .table-tr:eq("+ i +") .table-td-age").text(age)
-                    $(".table-content .table-tr:eq("+ i +") .table-td-first .years").text(firstTimeYears)
-                    $(".table-content .table-tr:eq("+ i +") .table-td-first .hours").text(firstTimeHours)
-                    $(".table-content .table-tr:eq("+ i +") .table-td-last .years").text(endTimeYears)
-                    $(".table-content .table-tr:eq("+ i +") .table-td-last .hours").text(endTimeHours)
-                    $(".table-content .table-tr:eq("+ i +") .table-td-frequency").text(percent)
-                    $(".table-content .table-tr:eq("+ i +") .table-td-count").text(count)
+                    $(".table-content .table-tr:eq("+ i +") .operating").attr({"data-id": id, "data-status": status})
+                    $(".table-content .table-tr:eq("+ i +") .table-td-name").text(groupName)
+                    $(".table-content .table-tr:eq("+ i +") .table-td-price .new-price").text(newPrice)
+                    $(".table-content .table-tr:eq("+ i +") .table-td-price .old-price del").text(oldPrice)
+                    $(".table-content .table-tr:eq("+ i +") .table-td-effective").text(effective)
+                    $(".table-content .table-tr:eq("+ i +") .table-td-organize").text(groupNum)
+                    $(".table-content .table-tr:eq("+ i +") .table-td-join").text(joinNum)
+                    $(".table-content .table-tr:eq("+ i +") .table-td-success").text(successNum)
+                    $(".table-content .table-tr:eq("+ i +") .table-td-used").text(usedNum)
+                    $(".table-content .table-tr:eq("+ i +") .table-td-notuse").text(unusedNum)
+                    $(".table-content .table-tr:eq("+ i +") .table-td-rate").text(rate)
+
                 }
             },
             error: function (ex) {
@@ -122,25 +98,51 @@
             }
         })
     }
-    // drawList();
+    drawList();
 
-    $(".page-down").click(function () {
-        if (page > 1) {
-            page--
-            drawList();
-            $(".page-number").text(page)
-        } else {
-            console.log("当前已是第一页")
-        }
+    var changeStatus = function (event1, event2, that) {
+        $.ajax({
+            url: 'http://shop.test/api/event/status',
+            type: 'put',
+            dataType: 'json',
+            data: {
+                id: event1,
+                status: event2
+            },
+            success: function (res) {
+                if (res.code != 200) {
+                    console.log(res.message);
+                } else {
+                    if (that.parent().attr("data-status") == 0) {
+                        that.parent().attr("data-status", 1)
+                        that.text("上架")
+                    } else {
+                        that.parent().attr("data-status", 0)
+                        that.text("下架")
+                    }
+                }
+            },
+            error: function (ex) {
+                console.log(ex)
+            }
+        })
+    }
+
+    $(".search-btn").on("click", function () {
+        keyword = $(".search-input").val()
+        drawList();
+        $(".search-input").val("")
     })
-    $(".page-up").click(function () {
-        if (page < pageCount) {
-            page++;
-            drawList();
-            $(".page-number").text(page)
-        } else {
-            console.log("已无更多数据")
-        }
+
+    $(".create-pdd").on("click", function () {
+        window.location.href = "/event/coupon/create"
+    })
+
+    $(".table-content").on("click", ".table-tr .operating .obtained, .table-tr .operating .shelf", function () {
+        var id = $(this).parent().attr("data-id")
+        var status = $(this).parent().attr("data-status")
+        var that = $(this)
+        changeStatus(id, status, that);
     })
 </script>
 @endsection
