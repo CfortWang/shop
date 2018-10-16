de3ec27fcd3d3caa4fd568fb6225720f03d69e0e
wood <jiumutou@126.com>
2018-10-16 17:54:10 +0800

    wood

--- app/Http/Controllers/Api/AdController.php
+++ app/Http/Controllers/Api/AdController.php
@@ -192,7 +192,7 @@ class AdController extends Controller
         );
         $validator = Validator::make($input, [
             'title'           => 'required',
-            'ad_image_file'   => 'required|image',
+            'ad_image_file'   => 'nullable|image',
             'landing_url'     => 'required|string',
             'start_date'      => 'required|date',
             'end_date'        => 'required|date',
