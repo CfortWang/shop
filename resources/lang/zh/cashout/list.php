a3282eb0fb2b87b091346dd824e6ba216a4c8008
wood <jiumutou@126.com>
2018-10-22 16:32:59 +0800

    wood

--- resources/lang/zh/cashout/list.php
+++ resources/lang/zh/cashout/list.php
@@ -1,11 +1,26 @@
 <?php
 return array
 (
-    'Amount'           => '金额',
-    'BuyerID'          => '商家ID',
-    'Bank'             => '银行',
-    'Holder'           => '持卡人',
-    'Account'          => '银行卡号',
-    'Register_Date'    => '申请日期',
-    'status'           => '状态'
+    'table' => [
+        'Amount'           => '金额',
+        'BuyerID'          => '商家ID',
+        'Bank'             => '银行',
+        'Holder'           => '持卡人',
+        'Account'          => '银行卡号',
+        'Register_Date'    => '申请日期',
+        'status'           => '状态',
+    ],
+    'model' => [
+        'title1'         =>'已驳回',
+        'msg1'           =>"驳回原由:",
+        'cancel'         =>'关闭',
+        'title2'         => '提现申请',
+        'availablePoint' => '可提现积分',
+        'amount'         => '金额',
+        'bankName'       => '银行',
+        'bankAccount'    => '银行卡号',
+        'bankAccountOwner' => '账户名',
+        'cashRequest'    => '申请'
+    ]
+
 );
---
+++ resources/lang/zh/point/list.php
@@ -0,0 +1,9 @@
+<?php
+return array
+(
+    'no'              => 'NO',
+    'description'     => '详情描述',
+    'point'           => '积分',
+    'remain'          => '剩余积分',
+    'created_at'      => '创建时间',
+);
